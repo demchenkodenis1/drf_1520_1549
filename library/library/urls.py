df2394f74e736ccd461b6dff45e6ff1ff6caa600
DenisDemchenko <demchenkodenis1@gmail.com>
2023-03-21 02:03:41 +0700

    lesson_6

--- library/library/urls.py
+++ library/library/urls.py
@@ -16,17 +16,19 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
-from rest_framework.routers import DefaultRouter
+from rest_framework.routers import DefaultRouter,SimpleRouter
 
-from authors.views import AuthorModelViewSet
-
+from authors.views import AuthorModelViewSet,BookModelViewSet,BiographyModelViewSet
+from rest_framework.authtoken import  views
 router = DefaultRouter()
 router.register('authors',AuthorModelViewSet)
-# router.register('books',AuthorModelViewSet)
+router.register('books',BookModelViewSet)
+router.register('biography',BiographyModelViewSet)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
     path('api/', include(router.urls)),
+    path('api-token-auth/', views.obtain_auth_token),
 
 ]
